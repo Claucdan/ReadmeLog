00518575910cc2cde7f2e4b5b8deb14688e2edb2
hhhh hgfh <dania.lukin@gmail.com>
2023-10-15 18:58:10 +0300

    Plugin work  - Select class clear  - Print class

--- src/main/java/LogDicsript.java
+++ src/main/java/LogDicsript.java
@@ -1,2 +1,20 @@
-public class LogDicsript {
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.ui.Messages;
+import org.jetbrains.annotations.NotNull;
+
+public class LogDicsript extends AnAction {
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent event) {
+        Editor editor = event.getData(PlatformDataKeys.EDITOR);
+        String message=editor.getSelectionModel().getSelectedText();
+        Messages.showMessageDialog(message,"Log Class",Messages.getInformationIcon());
+    }
+    @Override
+    public boolean isDumbAware() {
+        return false;
+    }
 }
