15dcaff177c9328c79d906d3d96aa5b36eeebc14
hhhh hgfh <dania.lukin@gmail.com>
2023-10-17 23:29:57 +0300

    Finally version
    Write to README.md

--- src/main/java/LogDicsript.java
+++ src/main/java/LogDicsript.java
@@ -5,13 +5,27 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.ui.Messages;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.*;
+
 public class LogDicsript extends AnAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent event) {
         Editor editor = event.getData(PlatformDataKeys.EDITOR);
-        String message=editor.getSelectionModel().getSelectedText();
-        Messages.showMessageDialog(message,"Log Class",Messages.getInformationIcon());
+        String message=String.valueOf(editor.getSelectionModel().getSelectedText());
+        String path= event.getProject().getBasePath();
+        path+="/docs/README.md";path=path.replace("/","\\");File file=new File(path);
+        if(message=="null")Messages.showMessageDialog("Pleas select class shat you want to add to readme", "Log Class", Messages.getInformationIcon());
+        else {
+            try {
+                FileWriter pw = new FileWriter(file, true);
+                pw.write("\n# New class\n" + "## " + message);
+                pw.flush();
+                Messages.showMessageDialog(message, "Log Class", Messages.getInformationIcon());
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
     }
     @Override
     public boolean isDumbAware() {
